77e0aad2e0a0d7094ac528c6f096c7ca74eeaf8a
Matteo Castellarin <matteo.castellarin@esma.europa.eu>
2018-11-08 14:59:28 +0100

    Delivery Pipeline groovy sample

---
+++ vars/basicDeliveryPipeline.groovy
@@ -0,0 +1,41 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+        agent any
+        stages {
+
+            stage('build') {
+                steps {
+                    echo "Performing build: [${pipelineParams.buildMessageSample}]"
+                }
+            }
+
+            stage ('test') {
+                steps {
+                    parallel (
+                            "unit tests": { sh "echo -e Unit tests message: ${pipelineParams.unitTestsMessage}" },
+                            "integration tests": { sh "echo -e Integration tests message: ${pipelineParams.integrationTestsMessage}" }
+                    )
+                }
+            }
+
+            stage('deploy'){
+                steps {
+                    echo "Performing deploy: [${pipelineParams.deployMessageSample}]"
+                }
+            }
+
+        }
+
+        post {
+            failure {
+                mail to: pipelineParams.email, subject: 'Pipeline failed', body: "Blah"
+            }
+        }
+    }
+}
